2e8589e52c3a76a7c0d5fa588f3adaa3f380d8cd
alexdarkovich <alexdarkovich@mail.ru>
2022-11-23 07:43:55 +0300

    programs implemented

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> set = new HashSet<>();
+
+        for (Map.Entry<String, List<String>> e : timetable.entrySet()) {
+            for (String iii : e.getValue()) {
+                set.add(iii);
+            }
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,68 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
+    public static void main(String[] args) {
+        Map<String, Integer> sourceMap = new HashMap<>();
+        sourceMap.put("Monday", 1);
+        sourceMap.put("Tuesday", 2);
+        sourceMap.put("Wednesday", 3);
+        sourceMap.put("Thursday", 4);
+        sourceMap.put("Friday", 5);
+
+        Set<String> str = new HashSet<>();
+        for (Map.Entry<String, Integer> e : sourceMap.entrySet()) {
+            str.add(e.getKey());
+        }
+        System.out.println(str);
+
+        Set<Integer> iii = new HashSet<>();
+        for (String sss : str){
+            iii.add(sss.length());
+        }
+        System.out.println(iii);
+
+        Map<Integer, Set<String>> mapToPrint = new HashMap<>();
+        for (Integer ii : iii){
+            Set<String> setForMap = new HashSet<>();
+            for (String ssss : str) {
+                if (ssss.length() == ii) {
+                    setForMap.add(ssss);
+                }
+            }
+            mapToPrint.put(ii, setForMap);
+        }
+
+        System.out.println(mapToPrint);
+
+    }
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Set<String> str = new HashSet<>();
+        for (Map.Entry<String, Integer> e : sourceMap.entrySet()) {
+            str.add(e.getKey());
+        }
+        System.out.println(str);
+
+        Set<Integer> iii = new HashSet<>();
+        for (String sss : str){
+            iii.add(sss.length());
+        }
+        System.out.println(iii);
+
+        Map<Integer, Set<String>> mapToPrint = new HashMap<>();
+        for (Integer ii : iii){
+            Set<String> setForMap = new HashSet<>();
+            for (String ssss : str) {
+                if (ssss.length() == ii) {
+                    setForMap.add(ssss);
+                }
+            }
+            mapToPrint.put(ii, setForMap);
+        }
+        return mapToPrint;
     }
 }
